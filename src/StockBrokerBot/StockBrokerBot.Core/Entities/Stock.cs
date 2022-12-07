fbda6fd3e0e0001fe345e85157751ac71c766dcd
dev <dev@example.com>
2022-12-07 06:50:44 +0000

    fix: add dynamodb annotations

--- src/StockBrokerBot/StockBrokerBot.Core/Entities/Stock.cs
+++ src/StockBrokerBot/StockBrokerBot.Core/Entities/Stock.cs
@@ -1,7 +1,11 @@
+using Amazon.DynamoDBv2.DataModel;
+
 namespace StockBrokerBot.Core.Entities;
 
+[DynamoDBTable("stock-prices")]
 public class Stock
 {
+    [DynamoDBHashKey]
     public string Name { get; set; }
     public decimal Price { get; set; }
 }
