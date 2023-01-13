5945b39d352f898627f5c104e30e4fff9273b71c
dev <dev@example.com>
2023-01-13 07:48:08 +0000

    chore: remove unused interface

--- src/StockBrokerBot/StockBrokerBot.ChatbotLambda/IntentProcessors/IIntentProcessor.cs
+++
@@ -1,9 +0,0 @@
-using Amazon.Lambda.Core;
-using Amazon.Lambda.LexV2Events;
-
-namespace StockBrokerBot.ChatbotLambda.IntentProcessors;
-
-public interface IIntentProcessor
-{
-    Task<LexV2Response> Process(LexV2Event lexEvent, ILambdaContext context);
-}
